c28062b49a55c1dbecb3e91e6cac65aa30a10d43
unknown <mateusteixeiradas@gmail.com>
2024-04-01 01:29:20 -0300

    Primeiro Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //runApp(const MyApp());
+  runApp(
+    const Column(
+  children: <Widget>[
+    Text('Deliver features faster', textDirection: TextDirection.ltr,),
+    Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
+    
+  ],
+)
+  );
+}
+
